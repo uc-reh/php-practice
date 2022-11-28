8846d278872f746d5f9d19202d8ea584e38f82cb
Abdul Rehman <abdul.rehman@ucertify.com>
2022-11-28 12:09:53 +0530

    Hello New Files

--- OOPs/49.php
+++ OOPs/49.php
@@ -1,18 +1,34 @@
 <?php
 //error handler function
-function customError($errno, $errstr)
-{
-    echo "<b>Error:</b> [$errno] $errstr<br>";
-    echo "Ending Script";
-    die();
+// function customError($errno, $errstr)
+// {
+//     echo "<b>Error:</b> [$errno] $errstr<br>";
+//     echo "Ending Script";
+//     die();
+// }
+
+// //set error handler
+// set_error_handler("customError", E_USER_WARNING);
+
+// //trigger error
+// $test = 2;
+// if ($test >= 1) {
+//     trigger_error("Value must be 1 or below", E_USER_WARNING);
+// }
+
+$n = 0;
+
+try{
+    if($n==0){
+        throw new Exception("Invalid Entry");
+    }
+    $div = $n / 5;
+    echo $div;
 }
-
-//set error handler
-set_error_handler("customError", E_USER_WARNING);
-
-//trigger error
-$test = 2;
-if ($test >= 1) {
-    trigger_error("Value must be 1 or below", E_USER_WARNING);
+catch(Exception $error){
+    echo $error;
+}
+finally{
+    echo "<br>Finally We are out from try and catch";
 }
 ?>
--- OOPs/52.php
+++ OOPs/52.php
@@ -1,8 +1,12 @@
-<?php
-function get_var($varname)
-{
-    global $$varname;
-    echo $$varname;
-}
+<html>
 
-show_var('_POST');
+<body>
+
+<form action="dataform.php" method="post">
+    Name : <input type="text" name="fname"><br><br>
+    Age : <input type="text" name="age"><br><br>
+    <input type="submit" name="save">
+</form>
+</body>
+
+</html>
